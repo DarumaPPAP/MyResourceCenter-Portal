2e46a307383c8402b9061e105459020aa308ec79
Daruma <haruto.0913.0105@gmail.com>
2026-08-17 19:24:19 +0900

    feat: add document viewer routing config

---
+++ assets/viewer-config.js
@@ -0,0 +1,7 @@
+window.MRCViewerConfig = Object.freeze({
+  largePdfThresholdBytes: 25 * 1024 * 1024,
+  adobeClientId: '',
+  adobeSdkUrl: 'https://acrobatservices.adobe.com/view-sdk/viewer.js',
+  adobeLocale: 'ja-JP',
+  adobeEnableLinearization: true
+});
